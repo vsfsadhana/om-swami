423b4a676ead906797ebd64aeb71101e7cdeb8bd
BahaaSamir <93657553+vsfsadhana@users.noreply.github.com>
2022-01-18 18:07:44 +0400

    Monk - Mobile Updates

--- src/inners.js
+++ src/inners.js
@@ -270,7 +270,7 @@ function init() {
 	window.addEventListener( 'resize', onWindowResize );
 	window.addEventListener( 'orientationchange', onOrientationChange);
 
-	// music()
+	music()
 
 	window.onblur = function(){
 
@@ -1103,6 +1103,40 @@ function monkPage(){
 			}
 
 		});
+
+	} else {
+
+
+		$(window).on('touchstart', function (e){
+
+			if(canScroll) {
+
+				ts = e.originalEvent.touches[0].clientX;
+
+			}
+
+		});
+
+		$(window).on('touchend', function (e){
+
+			if(canScroll) {
+
+				var te = e.originalEvent.changedTouches[0].clientX;
+
+				if(ts > te + 25){
+
+					nextSlide()
+
+				} else if(ts < te - 25){
+
+					prevSlide()
+
+				}
+
+			}
+
+		});
+
 
 	}
 
